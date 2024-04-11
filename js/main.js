04a83492dd8536b4d80fb805a5b29776208817cf
bericksmith <smi23001@byui.edu>
2024-04-11 08:49:00 -0700

    Setup weekly notes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // Define an array of links
 const links = [
-    { label: "Home", url: "index.html" },
-    { label: "About Me", url: "about.html" },
-    { label: "Projects", url: "projects.html" },
+    { label: "Week 1 Notes", url: "week1/index.html" },
+    { label: "Week 2 Notes", url: "week2/index.html" },
+    { label: "Week 3 Notes", url: "week3/index.html" },
+    { label: "Week 4 Notes", url: "week4/index.html" },
+    { label: "Week 5 Notes", url: "week5/index.html" },
+    { label: "Week 6 Notes", url: "week6/index.html" }
 ];
 
 // Create a function to dynamically generate list items
