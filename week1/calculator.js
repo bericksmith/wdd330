ba850dc863e859c1c51d4264bd437e20f65fa41f
bericksmith <smi23001@byui.edu>
2024-04-28 16:18:29 -0700

    week1-calculator-example-display-update

--- week1/calculator.js
+++ week1/calculator.js
@@ -1,34 +1,34 @@
-let calculator = {
-
-    read() {
-      this.a = +prompt('Enter the first number (a):', 0);
-      this.b = +prompt('Enter the second number (b):', 0);
-    },
-  
-    sum() {
-      return this.a + this.b;
-    },
-  
-    mul() {
-      return this.a * this.b;
-    },
-  
-    sub() {
-      return this.a - this.b;
-    },
-  
-    div() {
-      if (this.b !== 0) {
-        return this.a / this.b;
-      } else {
-        return 'Cannot divide by zero';
+document.addEventListener("DOMContentLoaded", function() {
+  const calculator = {
+      a: 0,
+      b: 0,
+      read() {
+          let firstNum = prompt('Enter the first number (a):', 0);
+          let secondNum = prompt('Enter the second number (b):', 0);
+          this.a = firstNum ? parseFloat(firstNum) : 0;
+          this.b = secondNum ? parseFloat(secondNum) : 0;
+      },
+      sum() {
+          return this.a + this.b;
+      },
+      mul() {
+          return this.a * this.b;
+      },
+      sub() {
+          return this.a - this.b;
+      },
+      div() {
+          return this.b !== 0 ? this.a / this.b : 'Cannot divide by zero';
       }
-    }
   };
-  
-  calculator.read();
-  alert(calculator.sum());
-  alert(calculator.mul());
-  alert(calculator.sub());
-  alert(calculator.div());
-  
+
+  function runCalculator() {
+      calculator.read();
+      alert(`Sum: ${calculator.sum()} 
+      Subtraction: ${calculator.sub()} 
+      Multiplication: ${calculator.mul()} 
+      Division: ${calculator.div()}`);
+  };
+
+  document.getElementById('start-calc').addEventListener('click', runCalculator);
+});
